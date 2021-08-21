17ee4bf1e9a410a5f428a8adf40aced7adfd4e47
ChaliceZero <2026992950@qq.com>
2021-08-21 22:32:56 +0800

    添加断言,过滤器配置

--- cloud-gateway-gateway9527/src/main/java/com/zq/learn/filter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/zq/learn/filter/MyLogGateWayFilter.java
@@ -22,17 +22,21 @@ public class MyLogGateWayFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("***********come in MyLogGateWayFilter:  " + new Date());
 
-        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname"); // 获取第一个参数名为uname的参数值
 
         if (uname == null) {
             log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
-            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
-            return exchange.getResponse().setComplete();
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE); // 设置返回的状态码
+            return exchange.getResponse().setComplete(); // 返回响应
         }
 
-        return chain.filter(exchange);
+        return chain.filter(exchange); // 放行，返回过滤链
     }
 
+    /**
+     * 加载的过滤器的顺序，数字越小，优先级越高
+     * @return
+     */
     @Override
     public int getOrder() {
         return 0;
